469928b0c8c37942902f45efadd21b2d506b13d3
Vasil Lazarov <vaskosound@abv.bg>
2013-08-21 02:54:08 -0700

    Change project url.

--- JustRunnerChat/JustRunnerChat/ChatScripts/main.js
+++ JustRunnerChat/JustRunnerChat/ChatScripts/main.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="controller.js" />
 /// <reference path="dataAccess.js" />
 (function () {
-    var serviceRoot = "http://roadrunnerchat.apphb.com/api/";
+    var serviceRoot = "http://multichannelschat.apphb.com/api/";
     //var serviceRoot = "http://localhost:16502/api/";
     
     var persister = Chat.persisters.get(serviceRoot);
